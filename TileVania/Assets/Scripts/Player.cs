aed6ded102abf69c47a5c005acb489b70062c7c5
trungepk <nevergiveup14896@gmail.com>
2018-10-19 14:14:57 +0700

    Update asset

---
+++ TileVania/Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+    [SerializeField] float moveSpeed = 5f;
+    Rigidbody2D myRigidbody;
+
+	void Start () {
+        myRigidbody = GetComponent<Rigidbody2D>();	
+	}
+	
+	void Update () {
+        Move();
+	}
+
+    private void Move()
+    {
+        var controlThrow = CrossPlatformInputManager.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        Vector2 playerVelocity = new Vector2(controlThrow, myRigidbody.velocity.y);
+        myRigidbody.velocity = playerVelocity;
+    }
+}
