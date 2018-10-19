61b56d3ca8ece59ef665b05d0301a7a57e44099d
trungepk <nevergiveup14896@gmail.com>
2018-10-19 21:30:37 +0700

    Transition to running anim

--- TileVania/Assets/Scripts/Player.cs
+++ TileVania/Assets/Scripts/Player.cs
@@ -8,22 +8,31 @@ public class Player : MonoBehaviour {
     [SerializeField] float moveSpeed = 5f;
     Rigidbody2D myRigidbody;
     SpriteRenderer playerSprite;
+    Animator animator;
 
 	void Start () {
         myRigidbody = GetComponent<Rigidbody2D>();
         playerSprite = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
 	}
 	
 	void Update () {
-        Move();
+        Run();
 	}
 
-    private void Move()
+    private void Run()
     {
         var controlThrow = CrossPlatformInputManager.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
         Vector2 playerVelocity = new Vector2(controlThrow, myRigidbody.velocity.y);
         myRigidbody.velocity = playerVelocity;
         FlipSprite();
+        TransitionToRunningAnimation();
+    }
+
+    private void TransitionToRunningAnimation()
+    {
+        bool isPlayerRunning = myRigidbody.velocity.x != 0;
+        animator.SetBool("Running", isPlayerRunning);
     }
 
     private void FlipSprite()
