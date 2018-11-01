c84be62f5303e283ad917cec7347ab447f256c99
trungepk <nevergiveup14896@gmail.com>
2018-11-01 22:04:13 +0700

    Add finish level condition

---
+++ TileVania/Assets/Scripts/LevelExit.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour {
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        StartCoroutine(LoadNextLevel());
+    }
+
+    IEnumerator LoadNextLevel()
+    {
+        yield return null;  // May changed depends on whatever animation put in
+        var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+}
